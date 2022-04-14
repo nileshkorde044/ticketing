95380d51d258ce964b57e9a964c5d396b0886e65
Nilesh Korde <nileshkorde44@gmail.com>
2022-04-13 22:04:51 -0500

    add test for signup

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true); // so express know app is behind ingress and can trust this https traffic
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test', // set cookie and use only when app is used over https
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true); // so express know app is behind ingress and can trust this https traffic
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true, // set cookie and use only when app is used over https
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
